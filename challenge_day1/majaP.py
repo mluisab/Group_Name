1a21b586bd5d6324db1e4ced7a979408f95db915
Maja <maja.kubara@student.uva.nl>
2023-09-04 17:00:19 +0200

    adding paragraphs

---
+++ challenge_day1/majaP.py
@@ -0,0 +1,13 @@
+from file_Damaris import *
+from file_Isis import *
+from file_maja import *
+from file_maria import *
+from file_mika import *
+from file_Rosa import *
+
+def P3():
+    print(f"One fateful day, as the frends gathered beneath the canal bridge at the heart of the university, they stumbled upon a wicked problem left by {wizard}. It challinged {anna}, {emily}, {luna}, {karen}, and {paula} unlike any problem bifore! With a playful radiance, they could feel its heavyness as it was EXTREMELY difficult to resolve.")
+def P9():
+    print(f"Tired and confused, they entered JK’s misty halls. {anna}, {emily}, {karen}, {luna}, and {paula} took the elevador to the second floor keeping {wizard}’s advice in mind. As they reached the study zpaces they realized that the Python beat them to the chase. The TAs were dead.")
+def P15():
+    print(f"With a combined effort, and plenty of free coffee,  they unlished a dazzling display of their powers, and finished the final delivirable which {anna}, {emily}, {karen}, {luna} and {paula} presented to the creatures and the wizard,{wizard}. Admiring their unity and strength, the creatures applauded and celibrated the group’s undeniable victory in not only killing the Python but also solving the wicked problem.")
