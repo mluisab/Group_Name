4fef4c8e00a45752e7f5638418438047e6f5de90
mluisab <marialuisa.baba@gmail.com>
2023-09-04 17:04:01 +0200

    Change

---
+++ challenge_day1/mariaP.py
@@ -0,0 +1,15 @@
+from file_Damaris import *
+from file_Isis import *
+from file_maja import *
+from file_maria import *
+from file_mika import *
+from file_Rosa import *
+
+def P2():
+    print(f"{anna}, the super essay tiper, had the power to generate essays within seconds! {emily}, the dibugger, had the ability to fix any issue by blinking. {luna}, the sleeper: could fall asleep and regain energy in the blink of an eye. {karen} is also a super typer but only writes drama about her friends. {paula}, the spectacular social scientist has the ability to recall every social feory she has lurned in her lifetime. The girls were brought together by {wizard}, who is the most powerful wizard on the UvA campus.")
+
+def P8():
+    print(f"Feeling puzzled by {wizard}, {anna} exclaimed “What should we do now?”. {karen} replied, “It is time to begin the kwuest. I have heard of these magical critures that live in JK. They call themselves TAs…” {emily}, {luna}, {anna} and {paula} all agreed and together, the girls went on a five minute walk to find these creatures.")
+
+def P14():
+    print(f"As the dais went by {anna}, {emily}, {karen}, {luna} and {paula}, began getting more tired and losing their powers, using their last drops of energi on the deliverable. The only solution to such a problem is free koffee. {wizard} recognized their struggle. Magically, a free coffee machine appeared. The girls were about to complete the challenge.")
