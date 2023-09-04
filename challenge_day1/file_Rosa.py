03b078cd1ec0d2ae3cb5e3ec2be5d6d20ec7341d
rosamuilu <rosamuilu@gmail.com>
2023-09-04 15:41:43 +0200

    committing my paragraphs

--- challenge_day1/file_Rosa.py
+++ challenge_day1/file_Rosa.py
@@ -11,3 +11,6 @@ karen = "Karen"
 
 def P1():
     print(f"Once upon a time, at the Universety of Amsterdam nestled by  the clear, blue canals, lived five extrordinary friends. Each had a unique gift bestowed upon them by the ancient CSSci gods. They were connected by the power of a wizard called {wizard}, and their bond was as strong as a student's desire to sleep. The group included {anna}, {emily}, {luna}, {karen}, {paula}.")
+
+def P7():
+    print(f"As they were finding methuds to collect and analyze data to solve this incredible issue, their work was attacked by a poisonous Python, eating lettes from their code. Now, {paula}, {luna}, {emily}, {anna} and {karen} were lost in a sea of programming errors, unsure of who to ask for help. Suddenly, a white light blinded the girls as {wizard} appeared from nowhere, telling them “Deal with it”.")
