912e4c866b37e630aa4609defbc5a7aec0aede45
mikashaham <126684667+mikashaham@users.noreply.github.com>
2023-09-04 16:38:19 +0000

    changes made mika's file

---
+++ challenge_day1/mikaP.py
@@ -0,0 +1,16 @@
+from file_Damaris import *
+from file_Isis import *
+from file_maja import *
+from file_maria import *
+from file_mika import *
+from file_Rosa import *
+
+def P6():
+    print(f"Days passed, and the friends, {luna}, {paula}, {karen}, {emily}, {anna}, remained confused in their mission to solve the wicked problem. They faced countless challenges and adversaries, but their unbreakable bond and CSSci powers always saw them through. Meanwhile, {wizard} was looking over them and giving them valuable feedback.")
+
+def P12():
+    print(f" 'AAAHH' screamed the Python. The intensity of the game was slowly killing him. As the girls were scoring more points, they won the battle. “FINALLY” {anna}, {emily}, {luna}, {karen}, and {paula} had defeated the vicious Python. They thanked {wizard} and moved on to their next Trello task.")
+
+def P18(): 
+    print(f"Despite their fame and the allure of their incredible bond, the seven friends, {anna}, {emily}, {karen}, {luna} and {paula}, remained humble. They knew that their true power lay in their unity and their unwavering friendship. Guided by {wizard}, they were able to solve many more wicked problems.")
+
