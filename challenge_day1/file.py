ed58dc47d12d3b5d6e1e5ba51d53ce33a4cce99f
rosamuilu <rosamuilu@gmail.com>
2023-09-04 15:07:34 +0200

    added other stuff

--- challenge_day1/file.py
+++ challenge_day1/file.py
@@ -9,6 +9,3 @@ from file import *
 def Introduction():
     print("This is Team YourTeamNameHere. We are:")
 
-def P1():
-    print(f"Once upon a time, at the Universety of Amsterdam nestled by  the clear, blue canals, lived five extrordinary friends. Each had a unique gift bestowed upon them by the ancient CSSci gods. They were connected by the power of a wizard called {wizard}, and their bond was as strong as a student's desire to sleep. The group included {anna}, {emily}, {luna}, {karen}, {paula}.")
-
