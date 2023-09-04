687c7c1be6889577f6372b54fc1ca4986c3633cd
rosamuilu <rosamuilu@gmail.com>
2023-09-04 15:07:26 +0200

    added import commands

--- challenge_day1/file_mika.py
+++ challenge_day1/file_mika.py
@@ -1,3 +1,11 @@
+from file_Damaris import *
+from file_Isis import *
+from file_maja import *
+from file_mika import *
+from file_maria import *
+from file_Rosa import *
+from file import *
+
 def add_Mika(): 
     print("Mika")
 
