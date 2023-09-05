a1adfc0a2a7bd24b683b0c1e2ba4081476cfaf4b
rosamuilu <rosamuilu@gmail.com>
2023-09-05 14:59:20 +0200

    fixing errors

--- challenge_day1/DamarisP.py
+++ challenge_day1/DamarisP.py
@@ -6,10 +6,10 @@ from file_mika import *
 from file_Rosa import *
 
 def P1():
-    print(f"Once upon a time, at the University of Amsterdam nestled by the clear, blue canals, lived five extrordinary friends. Each had a unique gift bestowed upon them by the ancient CSSci gods. They were connected by the power of a wizard called {wizard}, and their bond was as strong as a student's desire to sleep. The group included {anna}, {emily}, {luna}, {karen}, {paula}.")
+    print(f"Once upon a time, at the University of Amsterdam nestled by the clear, blue canals, lived five extraordinary friends. Each had a unique gift bestowed upon them by the ancient CSSci gods. They were connected by the power of a wizard called {wizard}, and their bond was as strong as a student's desire to sleep. The group included {anna}, {emily}, {luna}, {karen}, {paula}.")
 
 def P7(): 
-    print(f"As they were finding methods to collect and analyze data to solve this incredible issue, their work was attacked by a poisonous Python, eating lettes from their code. Now, {paula}, {luna}, {emily}, {anna} and {karen} were lost in a sea of programming errors, unsure of who to ask for help. Suddenly, a white light blinded the girls as {wizard} appeared from nowhere, telling them “Deal with it”.")
+    print(f"As they were finding methods to collect and analyze data to solve this incredible issue, their work was attacked by a poisonous Python, eating letters from their code. Now, {paula}, {luna}, {emily}, {anna} and {karen} were lost in a sea of programming errors, unsure of who to ask for help. Suddenly, a white light blinded the girls as {wizard} appeared from nowhere, telling them “Deal with it”.")
 
 def P13():
-    print(f"Now that {anna}, {emily}, {luna}, {karen} and {paula} won the battle, the missing pieces of code magically returned to their places, and the TAs came back to life. “Good job!” said {wizard}. They were finally able to begin writing their final deliverable! just in time for their partnur pitch.")
+    print(f"Now that {anna}, {emily}, {luna}, {karen} and {paula} won the battle, the missing pieces of code magically returned to their places, and the TAs came back to life. “Good job!” said {wizard}. They were finally able to begin writing their final deliverable! Just in time for their partner pitch.")
--- challenge_day1/majaP.py
+++ challenge_day1/majaP.py
@@ -6,8 +6,8 @@ from file_mika import *
 from file_Rosa import *
 
 def P3():
-    print(f"One fateful day, as the friends gathered beneath the canal bridge at the heart of the university, they stumbled upon a wicked problem left by {wizard}. It challinged {anna}, {emily}, {luna}, {karen}, and {paula} unlike any problem bifore! With a playful radiance, they could feel its heavyness as it was EXTREMELY difficult to resolve.")
+    print(f"One fateful day, as the friends gathered beneath the canal bridge at the heart of the university, they stumbled upon a wicked problem left by {wizard}. It challenged {anna}, {emily}, {luna}, {karen}, and {paula} unlike any problem before! With a playful radiance, they could feel its heavyness as it was EXTREMELY difficult to resolve.")
 def P9():
-    print(f"Tired and confused, they entered JK’s misty halls. {anna}, {emily}, {karen}, {luna}, and {paula} took the elevator to the second floor keeping {wizard}’s advice in mind. As they reached the study zpaces they realized that the Python beat them to the chase. The TAs were dead.")
+    print(f"Tired and confused, they entered JK’s misty halls. {anna}, {emily}, {karen}, {luna}, and {paula} took the elevator to the second floor keeping {wizard}’s advice in mind. As they reached the study spaces they realized that the Python beat them to the chase. The TAs were dead.")
 def P15():
-    print(f"With a combined effort, and plenty of free coffee,  they unleashed a dazzling display of their powers, and finished the final delivirable which {anna}, {emily}, {karen}, {luna} and {paula} presented to the creatures and the wizard,{wizard}. Admiring their unity and strength, the creatures applauded and celibrated the group’s undeniable victory in not only killing the Python but also solving the wicked problem.")
+    print(f"With a combined effort, and plenty of free coffee, they unleashed a dazzling display of their powers, and finished the final deliverable which {anna}, {emily}, {karen}, {luna} and {paula} presented to the creatures and the wizard, {wizard}. Admiring their unity and strength, the creatures applauded and celebrated the group’s undeniable victory in not only killing the Python but also solving the wicked problem.")
