f1b0284cccd5c951c574089554e611c7f3f620cd
mluisab <marialuisa.baba@gmail.com>
2023-09-05 10:50:16 +0200

    change

--- challenge_day1/mariaP.py
+++ challenge_day1/mariaP.py
@@ -6,7 +6,7 @@ from file_mika import *
 from file_Rosa import *
 
 def P2():
-    print(f"{anna}, the super essay typer, had the power to generate essays within seconds! {emily}, the debugger, had the ability to fix any issue by blinking. {Luna}, the sleeper: could fall asleep and regain energy in the blink of an eye. {karen} is also a super typer but only writes drama about her friends.{paula}, the spectacular social scientist has the ability to recall every social theory she has lurned in her lifetime. The girls were brought together by {wizard}, who is the most powerful wizard on the UvA campus.")
+    print(f"{anna}, the super essay typer, had the power to generate essays within seconds! {emily}, the debugger, had the ability to fix any issue by blinking. {luna}, the sleeper: could fall asleep and regain energy in the blink of an eye. {karen} is also a super typer but only writes drama about her friends.{paula}, the spectacular social scientist has the ability to recall every social theory she has lurned in her lifetime. The girls were brought together by {wizard}, who is the most powerful wizard on the UvA campus.")
 
 def P8():
     print(f"Feeling puzzled by {wizard}, {anna} exclaimed “What should we do now?”. {karen} replied, “It is time to begin the quest. I have heard of these magical creatures that live in JK. They call themselves TAs…” {emily}, {luna}, {anna} and {paula} all agreed and together, the girls went on a five minute walk to find these creatures.")
