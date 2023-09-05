107c5e87a8eb2fcd01d0b5e2d9c86793d875bd2e
rosamuilu <rosamuilu@gmail.com>
2023-09-05 14:55:09 +0200

    my paragraphs

---
+++ challenge_day1/RosaP.py
@@ -0,0 +1,16 @@
+from file_Damaris import *
+from file_Isis import *
+from file_maja import *
+from file_maria import *
+from file_mika import *
+from file_Rosa import *
+
+def P5():
+    print(f"They discovered that Luna, with her power of sleep, had dreamt of the weekly goals they had to establish to defeat the problem given to them by {wizard}. {anna}, {emily}, {karen}, and {paula} started discussing the most appropriate method of data collection.")
+
+def P11():
+    print(f"Prepared for battle,{emily}, {anna}, {karen}, {luna} and {paula} entered THE COMMON ROOM. They heard a “sssssss….” coming from the foosball table. Once again, {wizard} appeared exclaiming “To win Python you must play!” The ultimate Fussball battle began with the wizard as the referee.")
+
+def P1():
+    print(f"However it wasn't the project’s power that defined their greatness but the teamwork and friendship they shared. Their bond remained stronger than ever, where {anna}, {emily}, {karen}, {luna} and {paula} along with the wizard {wizard} continued to inspire awe and wonder for generations to come.")
+          
