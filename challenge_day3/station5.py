56aae0a10868faaae8783ece279d8bb25050ed33
mluisab <marialuisa.baba@gmail.com>
2023-09-06 13:54:38 +0200

    change

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -13,12 +13,9 @@ def solution_station_5(station5_input):
 
 station5_input = input("Enter a name: ")
 station5_output = solution_station_5(station5_input)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> a0195fdc006ab3a39557f4785009c3dcaac08ee9
+
 print(f"Team for {station5_input}: {station5_output}")
-=======
-print(f"Team for {station5_input}: {station5_output}")
->>>>>>> d684b8da1c9b12c240c5bef89019a2baf82bfcf8
+
+
+
