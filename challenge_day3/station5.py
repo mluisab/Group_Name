030509f19eac6a83c6122c61ac68f1bc97633de8
damarisparasca <126685870+damarisparasca@users.noreply.github.com>
2023-09-06 13:39:37 +0200

    Update station5.py

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -11,6 +11,7 @@ def solution_station_5(station5_input):
     except ValueError:
         return "Name not found"  
 
-station5_input = "Isis"  
+station5_input = input("Enter a name: ")
 station5_output = solution_station_5(station5_input)
+
 print(f"Team for {station5_input}: {station5_output}")
