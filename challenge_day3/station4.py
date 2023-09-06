2db3379845a71bcf189f217570e689ec7f401d41
isissris <isissris@gmail.com>
2023-09-06 12:59:10 +0200

    sdfds

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,11 +1,7 @@
-x = int(input("Enter a number: "))
-
-def solution_station_4(numb):
-    if numb%2>0:
+def solution_station_4(output4):
+    if output4%2>0:
         print(True)
     else:
         print(False)
-    return numb
-
-numb = solution_station_4(x)
+    return output4
 
