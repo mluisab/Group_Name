3d855529c7b3ece2e304830ed87807dce4ccc226
mluisab <marialuisa.baba@gmail.com>
2023-09-06 14:12:56 +0200

    change

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,6 +1,11 @@
+equation = int(input("Enter an equation: "))
+a=3
+b=-1
+c=4
+d=7
+e=0.5
+
 def solution_station_7():
-    a=3
-    b=-1
-    c=4
-    d=7
-    e=0.5
+   answer= eval(equation, {'a':a, 'b':b, 'c':c, 'd':d, 'e':e})
+   return answer
+
