a450299770f1fcc48759b5a560a039aea4b381c3
isissris <isissris@gmail.com>
2023-09-06 12:32:40 +0200

    jknjk

---
+++ challenge_day3/station4.py
@@ -0,0 +1,11 @@
+x = int(input("Enter a number: "))
+
+def solution_station_4(numb):
+    if numb%2>0:
+        print(True)
+    else:
+        print(False)
+    return numb
+
+numb = solution_station_4(x)
+
