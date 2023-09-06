98361e5a76308fbf67a39e2568edf5b1a8602cb6
Maja <maja.kubara@student.uva.nl>
2023-09-06 15:55:05 +0200

    done

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,4 +1,6 @@
 def solution_station_4(numb):
+    if numb==1:
+        return False
     for i in range(2, numb):
         if (numb%i)==0:
             return False
--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -1,5 +1,4 @@
 import math
 from math import sin
 def solution_station_6(x):
-    return math.sin(x)
-
+    return round(math.sin(x),4)
